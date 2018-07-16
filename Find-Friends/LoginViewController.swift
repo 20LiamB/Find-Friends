11b1440515b98e2faa4606f5cbf3fcbacc0074cc
Liam Bakar <20liamb@students.harker.org>
2018-07-16 13:32:55 -0700

    Added MapViewController and a MapKitView

---
+++ Find-Friends/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Find-Friends
+//
+//  Created by Liam Bakar on 7/16/18.
+//  Copyright © 2018 Liam Bakar. All rights reserved.
+//
+
+import Foundation
---
+++ Find-Friends/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  Find-Friends
+//
+//  Created by Liam Bakar on 7/16/18.
+//  Copyright © 2018 Liam Bakar. All rights reserved.
+//
+
+import Foundation
+
+
+class MapViewController{
+    
+}
